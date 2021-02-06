514ceb2c8642182e2af1322585ab6a1f59c92ffa
MahenPD <61178225+MahenPD@users.noreply.github.com>
2021-02-06 12:40:52 +0530

    payment gateway with stripe implemented

--- src/App.js
+++ src/App.js
@@ -4,8 +4,36 @@ import Home from "./components/Home";
 import Header from "./components/Header";
 import Checkout from "./components/Checkout";
 import Login from "./components/Login";
+import { useEffect } from "react";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import Payment from "./components/Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51IHkLFBa5YSiZO6pGCliVbnJJAhGUcBA0N2aaZP7cfLa2GyRhVOWAUDrht9EWdGRpttM9LehImpSf2LrtrrkjXnD00MXX83rY1"
+);
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <div className="app">
@@ -16,6 +44,12 @@ function App() {
           <Route path="/checkout">
             <Header />
             <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
